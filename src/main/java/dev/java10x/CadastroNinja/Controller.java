73b894e04ca20650c5bc879451a94cd8e04bc4a2
hack-perce <davidejesus2807@gmail.com>
2025-07-18 02:52:40 -0300

    Criado a controller

---
+++ src/main/java/dev/java10x/CadastroNinja/Controller.java
@@ -0,0 +1,23 @@
+package dev.java10x.CadastroNinja;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //controlador para API's Rest
+@RequestMapping //torna mapeado = sempre usamos ele dps do @RestController
+public class Controller {
+
+    @GetMapping("/boasVindas") //ele pega informações   //especificando a rota dele
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota"; //resultado para todos que acessarem essa rota
+    }
+
+}
+
+/*
+* @PostMapping - manda informações
+* @PutMapping - alterar informações
+* @PatchMapping - "
+* @DeleteMapping - deletar informações
+* */
